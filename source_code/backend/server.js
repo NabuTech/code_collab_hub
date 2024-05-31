3b3a46a675a868beaff289704673678ef84f3590
TC_win_laptop <88120195+TcPirate1@users.noreply.github.com>
2024-05-31 12:42:00 +1200

    Establish connection to MongoDB

--- source_code/backend/server.js
+++ source_code/backend/server.js
@@ -1,1 +1,10 @@
 // server.js
+import { MongoClient } from "mongodb";
+
+const url = process.env.connection_string;
+const client = new MongoClient(url);
+
+async function connect() {
+  await client.connect();
+  console.log("Connected to MongoDB");
+}
